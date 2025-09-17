421c7ba78dc2b5d2db01cd9fb265c80b31d9ae6d
Drew Pawluckie <drewpawluckie@gmail.com>
2025-09-16 23:01:02 -0400

    XLSX formatting

--- JobAppTracker/Export/XLSXExporter.py
+++ JobAppTracker/Export/XLSXExporter.py
@@ -1,7 +1,7 @@
 from datetime import date
 from typing import Callable, Any
 
-from Data.Application import Application, Status
+from Data.Application import Application, Status, JobTypes
 from Export.BaseExporter import BaseExporter
 
 import xlsxwriter as xlw
@@ -23,7 +23,7 @@ class XLSXExporter(BaseExporter):
                 get_interview_count_for_application(self.currentFile,
                                                     app.app_id)
             ),
-            (h.H_JOB_TYPE, lambda app: str(app.job_type)),
+            (h.H_JOB_TYPE, lambda app: app.job_type),
             (h.H_LOCATION, lambda app: app.location),
             (h.H_APP_SRC, lambda app: app.found_at),
             (h.H_APP_WEBSITE, lambda app: app.website),
@@ -31,8 +31,7 @@ class XLSXExporter(BaseExporter):
             (h.H_MATERIALS, lambda app: app.materials),
             (h.H_SALARY, lambda app: app.salary),
             (h.H_STATUS, lambda app: ghost_prediction(self.currentFile, app)),
-            (h.H_COMMENT, lambda app: app.comments),
-            (h.H_PENDING, lambda app: str(app.days_pending))
+            (h.H_PENDING, lambda app: app.days_pending)
         ]
 
     @property
@@ -43,24 +42,25 @@ class XLSXExporter(BaseExporter):
         self.currentFile = currentFile
 
     def _export(self, data: list[Application]) -> str:
-        with xlw.Workbook("Tempname.xlsx") as workbook:
+        with xlw.Workbook("Tempname.xlsx") as wb:
 
             # Create cell formats
-            bold = workbook.add_format({"bold": True})
-            italic = workbook.add_format({"italic": True})
-            date_format = workbook.add_format(
+            header_fmt = wb.add_format({"bold": True, "fg_color": "#bdbdbd"})
+            italic = wb.add_format({"italic": True})
+            date_format = wb.add_format(
                 {"num_format": "d mmm yyyy"}
             )
-            red_cell = workbook.add_format({
+            red_cell = wb.add_format({
                 "fg_color": "red",
                 "font_color": "white"})
-            org_cell = workbook.add_format({
+            org_cell = wb.add_format({
                 "fg_color": "orange",
                 "font_color": "white"})
-            # yel_cell = workbook.add_format({"bg_color": "yellow"})
-            mid_green_cell = workbook.add_format({"fg_color": "#93c47d"})
+            mid_yel_cell = wb.add_format({"bg_color": "#ffd966"})
+            mid_green_cell = wb.add_format({"fg_color": "#6aa84f"})
+            mid_light_blue = wb.add_format({"fg_color": "#6d9eeb"})
 
-            main_worksheet = workbook.add_worksheet("Applications")
+            main_ws = wb.add_worksheet("Applications")
 
             note = f"""Exported from Job Application Tracker on {
                 date.today().strftime('%B %d, %Y')
@@ -70,10 +70,10 @@ class XLSXExporter(BaseExporter):
             header_row = 2
             beginning_row = 3
 
-            main_worksheet.write(note_row, 0, note, italic)
+            main_ws.write(note_row, 0, note, italic)
 
             for col, (header, _) in enumerate(self._columns):
-                main_worksheet.write(header_row, col, header, bold)
+                main_ws.write(header_row, col, header, header_fmt)
 
             # Fill in data and format
             for row, app in enumerate(data):
@@ -82,39 +82,55 @@ class XLSXExporter(BaseExporter):
                     data = expr(app)
                     match data:
                         case date():
-                            main_worksheet.write(row + beginning_row, col, data, date_format)
+                            main_ws.write(row + beginning_row, col, data, date_format)
                         case Status.REJECTED:
-                            main_worksheet.write(row + beginning_row, col, str(data), red_cell)
+                            main_ws.write(row + beginning_row, col, str(data), red_cell)
                             currentStatus = data
                         case Status.LIKELY_GHOSTED:
-                            main_worksheet.write(row + beginning_row, col, str(data), org_cell)
+                            main_ws.write(row + beginning_row, col, str(data), org_cell)
+                            currentStatus = data
+                        case Status.INTERVIEW:
+                            main_ws.write(row + beginning_row, col, str(data), mid_light_blue)
                             currentStatus = data
                         case Status():
-                            main_worksheet.write(row + beginning_row, col, str(data))
+                            main_ws.write(row + beginning_row, col, str(data))
                             currentStatus = data
+                        case JobTypes.IN_PERSON:
+                            main_ws.write(row + beginning_row, col, str(data), mid_green_cell)
+                        case JobTypes.HYBRID:
+                            main_ws.write(row + beginning_row, col, str(data), mid_yel_cell)
+                        case JobTypes.REMOTE:
+                            main_ws.write(row + beginning_row, col, str(data), mid_light_blue)
                         case _:
-                            main_worksheet.write(row + beginning_row, col, data)
+                            main_ws.write(row + beginning_row, col, data)
+
+                # Get interview count and color in cell
+                int_count = get_interview_count_for_application(self.currentFile,
+                                                                app.app_id)
+                if int_count > 0:
+                    main_ws.write(row + beginning_row, 4, int_count, mid_green_cell)
 
                 # Match company cell color with app status color
                 match currentStatus:
                     case Status.REJECTED:
-                        main_worksheet.write(row + beginning_row,
+                        fmt = red_cell if int_count < 1 else mid_yel_cell
+                        main_ws.write(row + beginning_row,
                                              0,
                                              app.company,
-                                             red_cell)
+                                             fmt)
                     case Status.LIKELY_GHOSTED:
-                        main_worksheet.write(row + beginning_row,
+                        fmt = org_cell if int_count < 1 else mid_yel_cell
+                        main_ws.write(row + beginning_row,
                                              0,
                                              app.company,
-                                             org_cell)
+                                             fmt)
+                    case Status.INTERVIEW:
+                        main_ws.write(row + beginning_row,
+                                      0,
+                                      app.company,
+                                      mid_light_blue)
                     case _:
                         pass
-
-                # Color in other cells
-                int_count = get_interview_count_for_application(self.currentFile,
-                                                    app.app_id)
-                if int_count > 0:
-                    main_worksheet.write(row + beginning_row, 4, int_count, mid_green_cell)
 
 
 
